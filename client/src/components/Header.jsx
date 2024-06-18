2341ef539a3226321b9f488c5636ff60602e3aa4
anuj <anujsbhuyar@gmail.com>
2024-06-18 10:38:31 +0530

    signin and signup frontend done

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,30 +1,48 @@
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   return (
-    <div className='bg-slate-200'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-          <h1 className='font-bold'>Auth App</h1>
+    <div className="w-screen max-h-20 backdrop-blur-md shadow-md fixed grid grid-flow-col z-20">
+      {/* <div className="flex justify-end items-center max-w-6xl mx-auto p-3"> */}
+      <div className="flex justify-start items-center">
+        <Link to="/">
+          {/* <h1 className='font-bold'>Auth App</h1> */}
+          <img src="logo.png" alt="logo" className="h-14 max-h-16 m-1" />
         </Link>
-        <ul className='flex gap-4'>
-          <Link to='/'>
+      </div>
+      <div className=" flex justify-end">
+        <ul className="flex justify-end items-center lg:mx-5 md:mx-5 sm:mx-3">
+          <Link
+            className="rounded-full font-semibold  py-2 px-4 md:mr-5 lg:mr-5 sm:mr-1 border-black border shadow-md"
+            to="/"
+          >
             <li>Home</li>
           </Link>
-          <Link to='/about'>
+          <Link
+            className="rounded-full font-semibold  py-2 px-4 md:mr-5 lg:mr-5 sm:mr-1 border-black border shadow-md"
+            to="/about"
+          >
             <li>About</li>
           </Link>
-          <Link to='/profile'>
+          <Link
+            className="rounded-full font-semibold  py-2 px-4 md:mr-5 lg:mr-5 sm:mr-1 border-black border shadow-md"
+            to="/profile"
+          >
             {currentUser ? (
-              <img src={currentUser.profilePicture} alt='profile' className='h-7 w-7 rounded-full object-cover' />
+              <img
+                src={currentUser.profilePicture}
+                alt="profile"
+                className="h-7 w-7 rounded-full object-cover"
+              />
             ) : (
               <li>Sign In</li>
             )}
           </Link>
         </ul>
       </div>
+      {/* </div> */}
     </div>
   );
 }
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from '../redux/user/userSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import OAuth from '../components/OAuth';
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -22,10 +22,10 @@ export default function SignIn() {
     e.preventDefault();
     try {
       dispatch(signInStart());
-      const res = await fetch('/api/auth/signin', {
-        method: 'POST',
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -35,46 +35,53 @@ export default function SignIn() {
         return;
       }
       dispatch(signInSuccess(data));
-      navigate('/');
+      navigate("/");
     } catch (error) {
       dispatch(signInFailure(error));
     }
   };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input
-          type='email'
-          placeholder='Email'
-          id='email'
-          className='bg-slate-100 p-3 rounded-lg'
-          onChange={handleChange}
-        />
-        <input
-          type='password'
-          placeholder='Password'
-          id='password'
-          className='bg-slate-100 p-3 rounded-lg'
-          onChange={handleChange}
-        />
-        <button
-          disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
-        >
-          {loading ? 'Loading...' : 'Sign In'}
-        </button>
-        <OAuth />
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Dont Have an account?</p>
-        <Link to='/sign-up'>
-          <span className='text-blue-500'>Sign up</span>
-        </Link>
+    <div className="flex items-center justify-center min-h-screen bg-login ">
+      <div className="text-center backdrop-blur-md shadow-lg p-10 rounded-xl lg:w-1/3 md:w-1/3">
+        <h1 className="text-4xl font-bold mb-4">Sign In</h1>
+        <form onSubmit={handleSubmit} className="p-8 rounded">
+          {/* <label className=" text-gray-700">Email</label> */}
+
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            className="w-full p-2 border border-gray-300 shadow-sm rounded mt-1"
+            onChange={handleChange}
+          />
+          {/* <label className="block text-gray-700">Password</label> */}
+
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            className="w-full p-2 border border-gray-300 shadow-sm rounded mt-1"
+            onChange={handleChange}
+          />
+
+          <button
+            disabled={loading}
+            className="w-full p-2 border border-gray-300 shadow-sm rounded mt-1  px-6 py-2 text-sm font-medium hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            {loading ? "Loading..." : "Sign In"}
+          </button>
+          <OAuth />
+        </form>
+        <div className="flex gap-2 mt-5">
+          <p>Dont have an account?</p>
+          <Link to="/sign-up">
+            <span className="text-blue-500">Sign up</span>
+          </Link>
+        </div>
+        <p className="text-red-700 mt-5">
+          {error ? error.message || "Something went wrong!" : ""}
+        </p>
       </div>
-      <p className='text-red-700 mt-5'>
-        {error ? error.message || 'Something went wrong!' : ''}
-      </p>
     </div>
   );
 }
